fca921881ee4753d484934336a79ca4be72ba3eb
Zina A <keiraayari@gmail.com>
2024-07-15 11:45:47 +0200

    test

--- mytuto_django/polls/views.py
+++ mytuto_django/polls/views.py
@@ -17,32 +17,38 @@ from .models import Question
 
 from django.utils import timezone
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
+    
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions (not including those set to be
+        published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+    def index(request):
+        latest_question_list = Question.objects.order_by("-pub_date")[:5]
+        context = {"latest_question_list": latest_question_list}
+        return render(request, "polls/index.html", context)
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    def detail(request, question_id):
+        try:question = Question.objects.get(pk=question_id)
+        except Question.DoesNotExist:raise Http404("Question does not exist")
+        return render(request, "polls/detail.html", {"question": question})
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    def results(request, question_id):
+        esponse = "You're looking at the results of question %s."
+        return HttpResponse(response % question_id)
+
+
+    def vote(request, question_id):
+        return HttpResponse("You're voting on question %s." % question_id)
+
+
+
